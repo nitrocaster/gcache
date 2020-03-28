eaff21245556e495918d05d2363dfd456513de16
Pavel Kovalenko <su@nitrocaster.me>
2020-03-28 20:14:19 +0300

    GCacheCore: Add MD5 implementation.

---
+++ src/GCacheCore/MD5.hpp
@@ -0,0 +1,40 @@
+// MIT License
+// Copyright (c) 2020 Pavel Kovalenko
+
+#pragma once
+
+#include "Common/Config.hpp"
+#include "GCacheCore.hpp"
+#include <cstdint>
+#include <string>
+
+class GCACHECORE_API MD5
+{
+public:
+    struct GCACHECORE_API DigestType
+    {
+    public:
+        uint8_t Data[16] = {};
+        operator std::string() const;
+    };
+
+    MD5() { Init(); }
+    MD5 &Update(uint8_t const buf[], uint32_t length);
+    MD5 &Update(char const buf[], uint32_t length);
+    MD5 &Finalize();
+    DigestType Digest() const { return digest; }
+
+private:
+    static constexpr uint32_t BlockSize = 64;
+
+    void Init();
+    void TransformBlock(uint8_t const block[BlockSize]);
+    static void Decode(uint32_t output[], uint8_t const input[], uint32_t len);
+    static void Encode(uint8_t output[], uint32_t const input[], uint32_t len);
+
+    bool finalized;
+    uint8_t buffer[BlockSize];
+    uint32_t count[2];
+    uint32_t state[4];
+    DigestType digest;
+};
