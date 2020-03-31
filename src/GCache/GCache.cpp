abc554b06174654e991cc5325356692b2612b54b
Pavel Kovalenko <su@nitrocaster.me>
2020-04-01 02:02:46 +0300

    GCache: Handle exceptions.

--- src/GCache/GCache.cpp
+++ src/GCache/GCache.cpp
@@ -79,7 +79,7 @@ public:
             return path.lexically_normal();
         }
         while (false);
-        throw std::runtime_error("unrecognized cache entry: " + line);
+        throw std::runtime_error("unrecognized entry: " + line);
     }
 
     void Save(std::ofstream &fs, fs::path path) const
@@ -104,90 +104,126 @@ private:
 
     static std::string Hash(fs::path const &path)
     {
-        std::ifstream ifs(path, std::ios::binary);
-        MD5 md5;
-        md5.Update(ifs).Finalize();
-        return md5.Digest();
+        try
+        {
+            std::ifstream ifs(path, std::ios::binary);
+            MD5 md5;
+            md5.Update(ifs).Finalize();
+            return md5.Digest();
+        }
+        catch (...)
+        {
+            throw std::runtime_error("can't read file: " + path.string());
+        }
     }
 
     static int64_t Timestamp(fs::path const &path)
     {
-        auto ftime = fs::last_write_time(path);
+        std::error_code ec;
+        auto ftime = fs::last_write_time(path, ec);
+        if (ec)
+            throw std::runtime_error("can't read file timestamp: " + path.string());
         return ftime.time_since_epoch().count();
     }
 
     static void Timestamp(fs::path const &path, int64_t ts)
     {
+        std::error_code ec;
         auto newTime = fs::file_time_type(fs::file_time_type::clock::duration(ts));
-        fs::last_write_time(path, newTime);
+        fs::last_write_time(path, newTime, ec);
+        if (ec)
+            throw std::runtime_error("can't write file timestamp: " + path.string());
     }
 
 public:
     static constexpr char const *FileName = ".hash_cache.txt";
     
-    void Load(char const *root = ".")
+    void Reset()
     {
         files.clear();
+        modified = false;
+    }
+
+    void Load(char const *root = ".")
+    {
+        Reset();
         Log("* loading cache");
-        auto path = fs::path(root) / FileName;
-        if (fs::exists(path))
+        try
         {
-            std::ifstream ifs(path, std::ios::binary);
-            while (ifs.peek(), ifs.good())
+            auto path = fs::path(root) / FileName;
+            if (fs::exists(path))
             {
-                CacheEntry entry;
-                auto path = entry.Load(ifs).relative_path();
-                Log("*   " FPATH, path.c_str());
-                files[path] = entry;
+                std::ifstream ifs(path, std::ios::binary);
+                while (ifs.peek(), ifs.good())
+                {
+                    CacheEntry entry;
+                    auto path = entry.Load(ifs).relative_path();
+                    Log("*   " FPATH, path.c_str());
+                    files[path] = entry;
+                }
             }
         }
+        catch (std::exception e)
+        {
+            Reset();
+            Log("! error while loading cache: %s", e.what());
+            throw e;
+        }
         Log("* %u files cached", uint32_t(files.size()));
-        modified = false;
     }
     
     void Update(char const *root = ".")
     {
         Log("* updating cache");
         uint32_t ignored{}, checked{}, restored{}, updated{}, new_{};
-        for (RecursiveDirectoryIterator rec(root); rec; ++rec)
+        try
         {
-            auto path = rec.Path().relative_path().lexically_normal();
-            if (path.filename().c_str()[0] == '.')
+            for (RecursiveDirectoryIterator rec(root); rec; ++rec)
             {
-                Log("*   ignoring: " FPATH, path.c_str());
-                ignored++;
+                auto path = rec.Path().relative_path().lexically_normal();
+                if (path.filename().c_str()[0] == '.')
+                {
+                    Log("*   ignoring: " FPATH, path.c_str());
+                    if (rec.Directory())
+                        rec.Skip();
+                    ignored++;
+                    continue;
+                }
                 if (rec.Directory())
-                    rec.Skip();
-                continue;
-            }
-            if (rec.Directory())
-                continue;
-            auto &entry = files[path];
-            if (entry.Hash.empty())
-            {
-                Log("*   new file: " FPATH, path.c_str());
-                new_++;
-                entry.Hash = Hash(path);
-                entry.Timestamp = Timestamp(path);
-                continue;
-            }
-            Log("*   checking: " FPATH, path.c_str());
-            checked++;
-            auto ts = Timestamp(path);
-            if (ts == entry.Timestamp)
-                continue;
-            auto hash = Hash(path);
-            if (hash == entry.Hash)
-            {
-                Log("*   restoring timestamp: " FPATH, path.c_str());
-                restored++;
-                Timestamp(path, entry.Timestamp);
-                continue;
-            }
-            Log("*   updating: " FPATH, path.c_str());
-            updated++;
-            entry.Hash = hash;
-            entry.Timestamp = ts;
+                    continue;
+                auto &entry = files[path];
+                if (entry.Hash.empty())
+                {
+                    Log("*   new file: " FPATH, path.c_str());                    
+                    entry.Hash = Hash(path);
+                    entry.Timestamp = Timestamp(path);
+                    new_++;
+                    continue;
+                }
+                Log("*   checking: " FPATH, path.c_str());
+                auto ts = Timestamp(path);
+                checked++;
+                if (ts == entry.Timestamp)
+                    continue;
+                auto hash = Hash(path);
+                if (hash == entry.Hash)
+                {
+                    Log("*   restoring timestamp: " FPATH, path.c_str());                   
+                    Timestamp(path, entry.Timestamp);
+                    restored++;
+                    continue;
+                }
+                Log("*   updating: " FPATH, path.c_str());
+                entry.Hash = hash;
+                entry.Timestamp = ts;
+                updated++;
+            }            
+        }
+        catch (std::exception e)
+        {
+            Reset();
+            Log("! error while updating cache: %s", e.what());
+            throw e;
         }
         modified = updated || new_;
         Log("- update completed: ignored[%u], checked[%u], restored[%u], updated[%u], new[%u]",
@@ -199,9 +235,18 @@ public:
         if (!modified)
             return;
         Log("* saving cache");
-        std::ofstream ofs(fs::path(root) / FileName, std::ios::binary);
-        for (auto const &[path, entry] : files)
-            entry.Save(ofs, path);
+        try
+        {
+            std::ofstream ofs(fs::path(root) / FileName, std::ios::binary);
+            for (auto const &[path, entry] : files)
+                entry.Save(ofs, path);
+        }
+        catch (std::exception e)
+        {
+            Reset();
+            Log("! error while saving cache: %s", e.what());
+            throw e;
+        }
     }
 };
 
@@ -224,9 +269,16 @@ int main(int argc, char const **argv)
         Log("! unrecognized option (only --verbose is available)");
         return 1;
     }
-    Cache cache;
-    cache.Load();
-    cache.Update();
-    cache.Save();
+    try
+    {
+        Cache cache;
+        cache.Load();
+        cache.Update();
+        cache.Save();
+    }
+    catch (...)
+    {
+        return 1;
+    }
     return 0;
 }
